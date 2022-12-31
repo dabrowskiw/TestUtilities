8b8e0e752e47c1fdab57eba01c83a78dae37af9e
Wojtek Dabrowski <piotr.dabrowski@posteo.de>
2022-12-31 18:49:42 +0100

    Added method chain test

--- src/org/htw/prog1/testutils/DefaultTests.java
+++ src/org/htw/prog1/testutils/DefaultTests.java
@@ -65,6 +65,28 @@ public class DefaultTests {
         return null;
     }
 
+    /**
+     * Tests consecutive chains of methods. First, the object is constructed following cdata. Then, all methods from the first MethodData[]
+     * in mdata are run on the object. The result of the last method is expected to be an object, and on that object, all methods from the
+     * second MethodData[] in mdata are run. And so on and so forth.
+     * @param cdata
+     * @param mdatas
+     * @param roundDecimals
+     */
+    public void methodChainTest(ConstructorData cdata, MethodData[][] mdatas, int roundDecimals) {
+        try {
+            Constructor c = TestUtilities.getContructor(cdata.getKlass(), cdata.getArgumentKlasses());
+            Object obj = TestUtilities.getInstance(c, errorBuilder, cdata.getArguments());
+            for(MethodData[] mdata : mdatas) {
+                obj = methodListOnSameObjectTest(obj, mdata, roundDecimals, true);
+            }
+        } catch (TestException e) {
+            fail(e.getTestExceptionMessage());
+        } catch(Exception e) {
+            fail("Irgendetwas ist schief gelaufen: " + TestUtilities.getTraceString(e, 3));
+        }
+    }
+
     public Object methodListOnSameObjectTest(Object obj, MethodData[] mdatas, int roundDecimals, boolean returnLastMethodResult) {
         try {
             Object res = null;
